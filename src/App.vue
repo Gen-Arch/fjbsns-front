bea5299b67f84e970864d128f26b97518fe33d0f
gen <ymdtshm@gmail.com>
2020-09-22 23:54:48 +0900

    add card

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,52 @@
 <template>
   <v-app id="inspire">
-    <v-app-bar
-      app
-      clipped-left
-      color="red"
-      dense
-      >
+    <v-app-bar app clipped-left color="red" dense>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-        <v-toolbar-title class="mr-12 align-center">
-          <span class="title">FJB SNS</span>
-        </v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-row
-          align="center"
-          style="max-width: 650px"
-          >
-          <v-text-field
-            v-model="seachword"
-            :append-icon-cb="() => {}"
-            placeholder="Search..."
-            single-line
-            append-icon="mdi-magnify"
-            color="white"
-            hide-details
-            ></v-text-field>
-        </v-row>
+      <v-toolbar-title class="mr-12 align-center">
+        <span class="title">FJB SNS</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-row align="center" style="max-width: 650px">
+        <v-text-field
+          v-model="seachword"
+          :append-icon-cb="() => {}"
+          placeholder="Search..."
+          single-line
+          append-icon="mdi-magnify"
+          color="white"
+          hide-details
+        ></v-text-field>
+      </v-row>
     </v-app-bar>
 
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-      clipped
-      >
+    <v-navigation-drawer v-model="drawer" app clipped>
       <v-list dense>
-        <v-list-item
-          v-for="item in items"
-          :key="item.text"
-          link
-          >
+
+        <v-list-item v-for="item in items" :key="item.text" link>
           <v-list-item-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-action>
-      <v-list-item-content>
-        <v-list-item-title>
-          {{ item.text }}
-        </v-list-item-title>
-      </v-list-item-content>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.text }}</v-list-item-title>
+          </v-list-item-content>
         </v-list-item>
+
         <v-subheader class="mt-4 grey--text text--darken-1">SUBSCRIPTIONS</v-subheader>
+
         <v-list>
-          <v-list-item
-            v-for="item in users"
-            :key="item.name"
-            link
-            >
+          <v-list-item v-for="item in users" :key="item.name" link>
             <v-list-item-avatar>
               <img
-                :src="`https://avatars3.githubusercontent.com/u/${item.picture}`"
-                alt=""
-                >
+                :src="
+                  `https://avatars3.githubusercontent.com/u/${item.picture}`
+                "
+                alt
+              />
             </v-list-item-avatar>
-              <v-list-item-title v-text="item.name"></v-list-item-title>
+            <v-list-item-title v-text="item.name"></v-list-item-title>
           </v-list-item>
         </v-list>
-        <v-list-item
-          class="mt-4"
-          link
-          >
+        <v-list-item class="mt-4" link>
           <v-list-item-action>
             <v-icon color="grey darken-1">mdi-plus-circle-outline</v-icon>
           </v-list-item-action>
@@ -81,11 +61,8 @@
       </v-list>
     </v-navigation-drawer>
 
-
     <v-main>
-      <v-container class="fill-height">
-        <router-view></router-view>,
-      </v-container>
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
@@ -96,21 +73,24 @@ import { Component, Vue } from "vue-property-decorator";
 @Component({})
 export default class App extends Vue {
   // variuable
-  drawer: any = null
-  items: object[] =  [
-    { icon: 'mdi-trending-up', text: 'HOME' },
-    { icon: 'mdi-youtube-subscription', text: '通知' },
-    { icon: 'mdi-history', text: 'メッセージ' },
-    { icon: 'mdi-playlist-play', text: 'プロフィール' },
-  ]
+  drawer = true;
+  items: object[] = [
+    { icon: "mdi-trending-up", text: "HOME" },
+    { icon: "mdi-youtube-subscription", text: "通知" },
+    { icon: "mdi-history", text: "メッセージ" },
+    { icon: "mdi-playlist-play", text: "プロフィール" }
+  ];
 
   users: object[] = [
-    { picture: "15011235?s=460&u=5816071635b8f9a6c849e500835ee8ecc74e2448&v=4", name: 'Gen' },
-  ]
+    {
+      picture: "15011235?s=460&u=5816071635b8f9a6c849e500835ee8ecc74e2448&v=4",
+      name: "Gen"
+    }
+  ];
 
   // methods
-  created (): void {
-    this.$vuetify.theme.dark = true
+  created(): void {
+    this.$vuetify.theme.dark = true;
   }
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,71 @@
 <template>
-  <v-row
-    justify="center"
-    align="center"
-    >
-    <v-col class="shrink">
-      <v-tooltip right>
-        <template v-slot:activator="{ on }">
-          <v-btn
-            :href="source"
-            icon
-            large
-            target="_blank"
-            v-on="on"
+  <v-container fluid>
+    <v-row>
+      <v-col>
+        <v-textarea
+          name="input-7-1"
+          label="tweets"
+          value=""
+          hint="text fields at 140"
+          ></v-textarea>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-card
+          outlined
+          v-for="item in timeline"
+          :key="item.id"
+          >
+          <v-list-item
+            three-line
             >
-          </v-btn>
-        </template>
-    <span>Source</span>
-      </v-tooltip>
-    </v-col>
-  </v-row>
+            <v-list-item-avatar>
+              <img
+                :src="`${item.icon}`"
+                />
+            </v-list-item-avatar>
+            <v-list-item-content>
+              <div class="overline">{{ item.name }}</div>
+              <v-list-item-text>{{ item.tweet }}</v-list-item-text>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-card-actions>
+              <v-btn icon color="white" click="chat">
+                <v-icon>mdi-chat-plus</v-icon>
+              </v-btn>
+              <v-btn icon color="deep-orange">
+                <v-icon>mdi-thumb-up</v-icon>
+              </v-btn>
+              <v-btn icon color="indigo lighten-1">
+                <v-icon>mdi-thumb-down</v-icon>
+              </v-btn>
+              <v-btn icon color="green">
+                <v-icon>mdi-cached</v-icon>
+              </v-btn>
+          </v-card-actions>
+
+          <v-textarea
+            v-if="chat"
+            color="white"
+            hint="Hint text"
+            ></v-textarea>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class Home extends Vue {
+  chat: boolesn = false
+  timeline: object[] = [
+    {id: "0001", name: "Gen", icon: "https://avatars3.githubusercontent.com/u/15011235?s=460&u=5816071635b8f9a6c849e500835ee8ecc74e2449&v=4", tweet: "test tweet"},
+    {id: "0002", name: "Gen", icon: "https://avatars3.githubusercontent.com/u/15011235?s=460&u=5816071635b8f9a6c849e500835ee8ecc74e2449&v=4", tweet: "test tweet"}
+  ];
+}
+</script>
